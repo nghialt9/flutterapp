f3910925d47f50b0c37bdde075ab1ae5a8e03eeb
MY_PC <MY_PC@DESKTOP-E28TGCN>
2020-02-12 18:07:14 +0700

    add widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = Set<WordPair>();
@@ -38,7 +43,77 @@ class RandomWordsState extends State<RandomWords> {
         ),
         child: _buildSuggestions(), /* add child content here */
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _toggle, tooltip: 'Update', child: _getToggleChild()),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business), title: Text('Business')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.school), title: Text('School')),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.only(top: 60),
+          children: <Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  image: DecorationImage(
+                    image: AssetImage('images/nghia.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+              ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  bool toggle = true;
+  void _toggle() {
+    setState(() {
+      toggle = !toggle;
+    });
+  }
+
+  _getToggleChild() {
+    if (toggle) {
+      return Text('Toggle One');
+    } else {
+      return MaterialButton(onPressed: () {}, child: Text('Toggle Two'));
+    }
+  }
+
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   void _pushSaved() {
@@ -60,18 +135,19 @@ class RandomWordsState extends State<RandomWords> {
             context: context,
           ).toList();
           return Scaffold(
-            appBar: AppBar(
-              title: Text('Save Suggestions'),
-            ),
-            body: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('images/vt.jpeg'),
-                  fit: BoxFit.cover,),
+              appBar: AppBar(
+                title: Text('Save Suggestions'),
               ),
-              child: ListView(children: divided,),
-            )
-          );
+              body: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('images/vt.jpeg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: ListView(
+                    children: divided,
+                  )));
         },
       ),
     );
@@ -79,16 +155,17 @@ class RandomWordsState extends State<RandomWords> {
 
   Widget _buildSuggestions() {
     return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        },);
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
   }
 
   Widget _buildRow(WordPair pair) {
@@ -104,6 +181,7 @@ class RandomWordsState extends State<RandomWords> {
       ),
       onTap: () {
         setState(() {
+          // update UI
           if (alreadySaved) {
             _saved.remove(pair);
           } else {
@@ -113,9 +191,4 @@ class RandomWordsState extends State<RandomWords> {
       },
     );
   }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
 }
